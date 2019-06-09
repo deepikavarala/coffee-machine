ea176a34e29ceba06f98938ffe22d7ae0eff4a09
Deepika <34603038+diavarala@users.noreply.github.com>
2019-06-09 13:14:29 +0530

    Implemented Stage 2
    Now let's consider a case where you need a lot of coffee. Maybe, for example, you’re hosting a party with a lot of guests. The program should calculate how much water, coffee beans, and milk are necessary to make the specified amount of coffee. One cup of coffee made on this coffee machine contains 200 ml of water, 50 ml of milk, and 15 g of coffee beans.
    
    The user should input the amount of coffee he needs, in cups, for all the guests.
    
    Of course, all this coffee is not needed right now, so at this stage, the coffee machine doesn’t actually make any coffee.

---
+++ src/stage-2/CoffeeMachine.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CoffeeMachine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Write how many cups of coffee you will need: ");
+        int cups = scanner.nextInt();
+        System.out.println("For " + cups + " of coffee you will need:");
+        System.out.println(200 * cups + " ml of water");
+        System.out.println(50 * cups + " ml of milk");
+        System.out.println(15 * cups + " g of coffee beans");
+    }
+}
